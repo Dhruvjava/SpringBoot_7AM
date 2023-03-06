35105edfa2ee79e59d7a18473ba7b8d96280a52d
Dhruvjava <61408179+Dhruvjava@users.noreply.github.com>
2023-03-06 18:23:50 +0530

    Working with joins

--- 20SpringBootDataJoinsEx/src/main/java/com/cb/entity/Employee.java
+++ 20SpringBootDataJoinsEx/src/main/java/com/cb/entity/Employee.java
@@ -1,4 +1,23 @@
 package com.cb.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "employee_tab")
 public class Employee {
+
+    private Integer empId;
+
+    private String empName;
+
+    private Double empSal;
+
+    private Department dept;
 }
